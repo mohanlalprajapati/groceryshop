97525adec3fe0f6b126e280971ba1555a1b09151
mohanlal <mohan.h.prajapati@gmail.com>
2022-04-11 09:25:22 +0530

    Run application with single command

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -13,19 +13,25 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from django.utils.timezone import timedelta
 import os
+import environ
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR.parent, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=bjw^go41%x*=ho7ff1_ljen*u)v907*uk2y#(f&t*!c%q(jz4"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -58,6 +64,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
+    "http://127.0.0.1",
+    "http://frontend",
 ]
 
 ROOT_URLCONF = "BackEnd.urls"
@@ -86,14 +94,7 @@ WSGI_APPLICATION = "BackEnd.wsgi.application"
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
-        "PORT": 5432,
-    }
+    "default": env.db(),
 }
 
 
